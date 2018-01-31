8ea2014c59f40fbeeca76056a07fffbf64c53d6a
Eric Mink <eric@mink.li>
2018-01-31 20:23:14 +0000

    finished outlaying the help message.

--- makeAboveMeme.py
+++ makeAboveMeme.py
@@ -1,11 +1,20 @@
 """MakeAboveMeme
 
 Usage:
-    makeAboveMeme.py [-hv]
+    makeAboveMeme.py (-h | -v)
+    makeAboveMeme.py [-T <title>] [-t <text>] [-i <image>] [-o <output>] [--tags <tags>...] [[-p <points> -c <comments>] | -C <Ctext>]
 
 Options:
-    -h --help       Show this help message.
-    -v --version    Show the current version.
+    -h --help                       Show this help message.
+    -v --version                    Show the current version.
+    -T <title>, --title <title>     Specify the meme title.
+    -t <text>, --text <text>        Specify the text below the title and above the image.
+    -i <image>, --image <image>     Specify the relative or absolute path to your image.
+    -o <output>, --out <output>     The output file [default: ./aboveMeme.png]
+    --tags <tags>                   As many or few tags as you want.
+    -p <points>, --points <points>  How many points you want. Don't specify if you want me to not display any.
+    -c <comments>, --comments <comments>  How many comments there are below your post. Don't specify if you want mo to not display any.
+    -C <Ctext>                      Alternatively to -c and/or -p, specify the complete text to be displayed in the light-grey font.
 
 """
 from docopt import docopt
