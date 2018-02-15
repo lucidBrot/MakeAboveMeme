f4ad0e419d1ac18785e9e9fd52a8ce2e9073b749
Eric Mink <eric@mink.li>
2018-02-15 18:48:50 +0000

    changed help message for -l --link

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='MakeAboveMeme',
-    version='0.5.2',
+    version='0.5.3',
     packages=['mAm',],
     description="Use the commandline to create a simple text-above-image meme in the stlye of 9gag posts",
-    long_description=open('README.md').read(),
+    long_description=open('readme.txt').read(),
     author = 'Eric Mink aka LucidBrot',
     author_email = 'eric@mink.li',
     url = 'https://github.com/lucidBrot/MakeAboveMeme',
